7e0ebb8a95745e8deae73135b17d0b1ee8d7dc1c
Karthiksuthapalli <s.karthik0716@gmail.com>
2023-01-13 17:10:19 -0600

    karthikprograms

---
+++ Karthik/Oops/EncapDemo1.java
@@ -0,0 +1,61 @@
+package Karthik.Oops;
+
+ class EncapDemo {
+
+    private int i=100;
+    private String str="karthik";
+    private char ch='k';
+    // public String getStr() {
+    //     return str;
+    // }
+    // public void setStr(String str) {
+    //     this.str = str;
+    // }
+    // public char getCh() {
+    //     return ch;
+    // }
+    // public void setCh(char ch) {
+    //     this.ch = ch;
+    // }
+    // public Boolean getB() {
+    //     return b;
+    // }
+    // public void setB(Boolean b) {
+    //     this.b = b;
+    // }
+
+    // private Boolean b=true;
+
+    public int getI(){
+        return i;
+    }
+    public void setI(int i){
+       this.i= i;
+        
+
+    }
+
+    EncapDemo(){
+
+    }
+
+
+
+
+    
+
+    
+}
+public class EncapDemo1{
+ public static void main(String args[]){
+
+    EncapDemo oo=new EncapDemo();
+    oo.getI();
+   oo.setI(90);
+    System.out.println("the required number is "+oo.getI()+" "+"and later i changed mr number");
+
+    
+
+}
+
+}
